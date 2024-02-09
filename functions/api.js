ab77e2bf35f89cc452ceb8f040a66375f1b1fa18
Airly12000 <littlefrancis28@gmail.com>
2024-02-09 12:52:12 +0000

    route name change

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const serverless = require('serverless-http');
-const Invoices = require('../routes/Invoices');
+const express = require("express");
+const cors = require("cors");
+const serverless = require("serverless-http");
+const Invoices = require("../routes/Invoices");
 // const mysql = require('mysql2');
 // const path = require('path');
 // require('dotenv').config({ path: path.join(__dirname, '../', '.env') });
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Express Routes
-app.use('/api/routes', Invoices);
+app.use("/api/invoices", Invoices);
 
 module.exports.handler = serverless(app);
